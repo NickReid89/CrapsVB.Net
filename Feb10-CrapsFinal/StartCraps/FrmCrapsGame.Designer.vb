f1347ca7a380d5e151be8ba92f322f4b06f59c7b
Nickolas Reid <nickolaswdreid@gmail.com>
2013-08-26 21:35:17 -0300

    Final Craps Game

---
+++ Feb10-CrapsFinal/StartCraps/FrmCrapsGame.Designer.vb
@@ -0,0 +1,175 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Public Class FrmCrapsGame
+    Inherits System.Windows.Forms.Form
+
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Public Sub New()
+        MyBase.New()
+
+        'This call is required by the Windows Form Designer.
+        InitializeComponent()
+
+    End Sub
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overloads Overrides Sub Dispose(ByVal disposing As Boolean)
+        If disposing AndAlso components IsNot Nothing Then
+            components.Dispose()
+        End If
+        MyBase.Dispose(disposing)
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnPlay = New System.Windows.Forms.Button
+        Me.btnClear = New System.Windows.Forms.Button
+        Me.Label1 = New System.Windows.Forms.Label
+        Me.txtGames = New System.Windows.Forms.TextBox
+        Me.Label2 = New System.Windows.Forms.Label
+        Me.Label3 = New System.Windows.Forms.Label
+        Me.Label4 = New System.Windows.Forms.Label
+        Me.btnQuit = New System.Windows.Forms.Button
+        Me.lstDetail = New System.Windows.Forms.ListBox
+        Me.lstWinResults = New System.Windows.Forms.ListBox
+        Me.lstLossResults = New System.Windows.Forms.ListBox
+        Me.SuspendLayout()
+        '
+        'btnPlay
+        '
+        Me.btnPlay.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnPlay.Location = New System.Drawing.Point(26, 271)
+        Me.btnPlay.Name = "btnPlay"
+        Me.btnPlay.Size = New System.Drawing.Size(125, 40)
+        Me.btnPlay.TabIndex = 8
+        Me.btnPlay.Text = "Play"
+        '
+        'btnClear
+        '
+        Me.btnClear.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnClear.Location = New System.Drawing.Point(169, 271)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(125, 40)
+        Me.btnClear.TabIndex = 9
+        Me.btnClear.Text = "Clear"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 38)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(78, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Games to Play:"
+        '
+        'txtGames
+        '
+        Me.txtGames.Location = New System.Drawing.Point(96, 35)
+        Me.txtGames.Name = "txtGames"
+        Me.txtGames.Size = New System.Drawing.Size(70, 20)
+        Me.txtGames.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(244, 9)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(42, 16)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Wins"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(362, 9)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(58, 16)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Losses"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(35, 83)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(119, 16)
+        Me.Label4.TabIndex = 2
+        Me.Label4.Text = "Detail of Games"
+        '
+        'btnQuit
+        '
+        Me.btnQuit.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnQuit.Location = New System.Drawing.Point(312, 271)
+        Me.btnQuit.Name = "btnQuit"
+        Me.btnQuit.Size = New System.Drawing.Size(125, 40)
+        Me.btnQuit.TabIndex = 10
+        Me.btnQuit.Text = "Quit"
+        '
+        'lstDetail
+        '
+        Me.lstDetail.FormattingEnabled = True
+        Me.lstDetail.Location = New System.Drawing.Point(13, 102)
+        Me.lstDetail.Name = "lstDetail"
+        Me.lstDetail.Size = New System.Drawing.Size(179, 160)
+        Me.lstDetail.TabIndex = 11
+        '
+        'lstWinResults
+        '
+        Me.lstWinResults.FormattingEnabled = True
+        Me.lstWinResults.Location = New System.Drawing.Point(212, 41)
+        Me.lstWinResults.Name = "lstWinResults"
+        Me.lstWinResults.Size = New System.Drawing.Size(110, 212)
+        Me.lstWinResults.TabIndex = 12
+        '
+        'lstLossResults
+        '
+        Me.lstLossResults.FormattingEnabled = True
+        Me.lstLossResults.Location = New System.Drawing.Point(341, 41)
+        Me.lstLossResults.Name = "lstLossResults"
+        Me.lstLossResults.Size = New System.Drawing.Size(109, 212)
+        Me.lstLossResults.TabIndex = 13
+        '
+        'FrmCrapsGame
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(462, 323)
+        Me.Controls.Add(Me.lstLossResults)
+        Me.Controls.Add(Me.lstWinResults)
+        Me.Controls.Add(Me.lstDetail)
+        Me.Controls.Add(Me.btnQuit)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtGames)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnPlay)
+        Me.Name = "FrmCrapsGame"
+        Me.Text = "Craps Game"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnPlay As System.Windows.Forms.Button
+    Friend WithEvents btnClear As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents txtGames As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents btnQuit As System.Windows.Forms.Button
+    Friend WithEvents lstDetail As System.Windows.Forms.ListBox
+    Friend WithEvents lstWinResults As System.Windows.Forms.ListBox
+    Friend WithEvents lstLossResults As System.Windows.Forms.ListBox
+
+End Class
---
+++ Feb10-CrapsFinal/StartCraps/FrmCrapsGame.vb
@@ -0,0 +1,134 @@
+﻿' Fig 7.18: CrapsGame.vb - MODIFIED VERSION
+
+Public Class FrmCrapsGame
+    ' die-roll constants
+    Enum DiceNames
+        SNAKE_EYES = 2
+        TREY = 3
+        LUCKY_SEVEN = 7
+        CRAPS = 7
+        YO_LEVEN = 11
+        BOX_CARS = 12
+    End Enum
+
+    Enum GameStatus
+        LOSE
+        WIN
+        ROLLAGAIN
+        'by not assigning any values they will be 0, 1 and 2 respectively
+    End Enum
+    Const ROLL_COUNTER As Integer = 15
+
+
+    Private Sub btnPlay_Click(ByVal sender As System.Object, _
+        ByVal e As System.EventArgs) Handles btnPlay.Click
+        ' initialize variables for game
+        Dim myPoint As Integer ' total point
+        Dim sum As Integer, rollCount As Integer
+        Dim status As GameStatus = GameStatus.ROLLAGAIN
+        Dim games As Integer
+        Dim wins(ROLL_COUNTER) As Integer
+        Dim losses(ROLL_COUNTER) As Integer
+        'myPoint = 0
+        games = Convert.ToInt32(txtGames.Text)
+        For i = 1 To games
+            rollCount = 1
+            sum = RollDice() ' roll dice and calculate sum
+            ' check die roll
+            Select Case sum
+                Case DiceNames.LUCKY_SEVEN, DiceNames.YO_LEVEN
+                    status = GameStatus.WIN
+                Case DiceNames.SNAKE_EYES, DiceNames.TREY, DiceNames.BOX_CARS
+                    status = GameStatus.LOSE
+                Case Else
+                    status = GameStatus.ROLLAGAIN
+                    myPoint = sum
+            End Select
+            While status = GameStatus.ROLLAGAIN
+                sum = RollDice() ' roll dice and calculate sum
+                rollCount += 1  ' add one to the number of rolls in this game
+                ' check outcome of roll
+                If sum = myPoint Then ' win
+                    status = GameStatus.WIN
+                ElseIf sum = DiceNames.CRAPS Then ' lose
+                    status = GameStatus.LOSE
+                End If
+            End While
+            If status = GameStatus.LOSE Then
+                lstDetail.Items.Add("Lost in " & rollCount & " rolls.")
+                If rollCount < ROLL_COUNTER Then
+                    losses(rollCount) += 1
+                ElseIf rollCount >= ROLL_COUNTER Then
+                    losses(ROLL_COUNTER) += 1
+                End If
+                'lstDetail.Items.Insert(0, "Lost in " & rollCount & " rolls.")
+                'txtGameStatus.Text &= "Lost in " & rollCount & " rolls." & vbCrLf 
+            Else
+                lstDetail.Items.Add("Won in " & rollCount & " rolls.")
+                If rollCount < ROLL_COUNTER Then
+                    wins(rollCount) += 1
+                Else 'condion is irrelevant
+                    wins(ROLL_COUNTER) += 1
+                End If
+                'lstDetail.Items.Insert(0, "Won in " & rollCount & " rolls.")
+                'txtGameStatus.Text &= "Won in " & rollCount & " rolls." & vbCrLf
+            End If
+        Next
+        Dim winCount As Integer = 0
+        Dim lossCount As Integer = 0
+        For j = 1 To wins.Length - 1
+            winCount += wins(j)
+            lossCount += losses(j)
+            lstWinResults.Items.Add(j & "      " & wins(j))
+            lstLossResults.Items.Add(j & "     " & losses(j))
+        Next
+        lstWinResults.Items.Add("Won " & winCount & " games")
+        lstLossResults.Items.Add("Loss " & lossCount & " games")
+    End Sub ' btnPlay_Click
+
+
+    ' generate random die rolls
+    Function RollDice() As Integer
+        Dim myDie1 As Integer ' die1 face
+        Dim myDie2 As Integer ' die2 face
+        Static randomObject As New Random() ' generate random number
+        ' determine random integer
+        myDie1 = randomObject.Next(1, 7)
+        myDie2 = randomObject.Next(1, 7)
+        ' txtGameStatus.Text &= "Die1 = " & myDie1 & ", Die2 = " _
+        '     & myDie2 & vbCrLf
+        'lstDetail.Items.Add("     Die1 = " & myDie1 & ", Die2 = " _
+        '& myDie2)
+        'lstDetail.Items.Insert(0, "     Die1 = " & myDie1 & ", Die2 = " _
+        '                 & myDie2)
+        Return myDie1 + myDie2 ' return sum
+    End Function ' RollDice
+
+
+    Private Sub btnQuit_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnQuit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub btnClear_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnClear.Click
+        txtGames.Clear()
+        lstDetail.Items.Clear()
+        lstLossResults.Items.Clear()
+        lstWinResults.Items.Clear()
+        txtGames.Focus()
+    End Sub
+End Class ' FrmCrapsGame
+
+' **************************************************************************
+' * (C) Copyright 1992-2006 by Deitel & Associates, Inc. and               *
+' * Pearson Education, Inc. All Rights Reserved.                           *
+' *                                                                        *
+' * DISCLAIMER: The authors and publisher of this book have used their     *
+' * best efforts in preparing the book. These efforts include the          *
+' * development, research, and testing of the theories and programs        *
+' * to determine their effectiveness. The authors and publisher make       *
+' * no warranty of any kind, expressed or implied, with regard to these    *
+' * programs or to the documentation contained in these books. The authors *
+' * and publisher shall not be liable in any event for incidental or       *
+' * consequential damages in connection with, or arising out of, the       *
+' * furnishing, performance, or use of these programs.                     *
+' **************************************************************************
